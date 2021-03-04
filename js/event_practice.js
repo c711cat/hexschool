e9196717a412fc327ea8b4c17cbb6e15d48d7466
c711cat <c711cat@gmail.com>
2021-03-04 14:09:40 +0800

    onclick 與 addEventListener差別
    onclick不能同時綁定兩個事件，addEventListener可以同時綁2事件

--- js/event_practice.js
+++ js/event_practice.js
@@ -8,3 +8,31 @@ btn_el.addEventListener(
   },
   false
 )
+//onclick 與 addEventListener差別
+//onclick不能同時綁定兩個事件
+//使用onclick寫法，連續綁定兩個東西時，
+//只會顯示最後一筆，之前的onclick會被清除，靈活性低，所以有新的語法-addEventListener。
+var btnOn_el = document.querySelector('.btnOn')
+btnOn_el.onclick = function () {
+  alert('on-1')
+}
+btnOn_el.onclick = function () {
+  alert('on-2')
+}
+
+//addEventListener寫法:
+var btnAdd_el = document.querySelector('.btnAdd')
+btnAdd_el.addEventListener(
+  'click',
+  function () {
+    alert('addon-1')
+  },
+  false
+)
+btnAdd_el.addEventListener(
+  'click',
+  function () {
+    alert('addon-2')
+  },
+  false
+)
