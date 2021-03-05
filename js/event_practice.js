f4dc9e213ac3ab1525df9ec41f8a9774684365b3
c711cat <c711cat@gmail.com>
2021-03-05 11:53:06 +0800

    嘗試e ， e.target ， e.target.nodeName 語法使用的結果差別

--- js/event_practice.js
+++ js/event_practice.js
@@ -68,3 +68,14 @@ link_el.addEventListener('click', function (e) {
   e.preventDefault()
   console.log('test')
 })
+
+var header_el = document.querySelector('.header')
+header_el.addEventListener(
+  'click',
+  function (e) {
+    // console.log(e) → 可以從網頁的console去看到點擊的詳細內容:滑鼠點擊的座標、及event的object(target:li)
+    // console.log(e.target) → 看到點到的內容，例如: <li><a href="#">123</a></li> 或 <a href="#">123</a>
+    console.log(e.target.nodeName) //→ 看到點到的標籤名稱，例如:UL、LI、A
+  },
+  false
+)
