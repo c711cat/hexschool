4f3ce15e09c4a22f394acc9ce88e107957e30214
c711cat <c711cat@gmail.com>
2021-03-06 14:11:56 +0800

    kakas題目2 - 0306

---
+++ js/kakas_0306_2.js
@@ -0,0 +1,13 @@
+function compare(a, b) {
+  if () {
+    console.log('a 等於 b')
+  } else {
+    console.log('a 大於 b')
+  } else {
+    console.log('a 小於 b')
+  }
+}
+
+compare(1, 1) // a 等於 b
+compare(1, 2) // a 小於 b
+compare(1, -1) //a 大於 b
