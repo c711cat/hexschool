70906cadaccb7bfd6918d4820013dd735d120349
c711cat <c711cat@gmail.com>
2021-03-11 14:05:09 +0800

    compare('111', '111')  // 一樣，compare('111', '222') // 不一樣，3/6做一次3/10再做一次

---
+++ js/kakas/kakas_0306.js
@@ -0,0 +1,12 @@
+//第1題- 3/6，3/10
+// compare('111', '222') // 不一樣
+// compare('111', '111') // 一樣
+
+function compare (a, b) {
+  if (a == b) {
+    console.log('一樣')
+  }
+  if (a !== b) {
+    console.log('不一樣')
+  }
+}
