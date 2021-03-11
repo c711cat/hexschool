f99bea0015d8e2bb1bcabe401fddf14fda6307e3
c711cat <c711cat@gmail.com>
2021-03-11 14:05:53 +0800

    第2題 3/6做一次，3/10再做一次
    // compare(1, 1) // a 等於 b
    // compare(1, 2) // a 小於 b
    // compare(1, -1) //a 大於 b

---
+++ js/kakas/kakas_0306_2.js
@@ -0,0 +1,14 @@
+// 第2題 3/6，3/10
+// compare(1, 1) // a 等於 b
+// compare(1, 2) // a 小於 b
+// compare(1, -1) //a 大於 b
+
+function compare (a, b) {
+  if (a == b) {
+    console.log('a 等於 b')
+  } else if (a < b) {
+    console.log('a 小於 b')
+  } else if (a > b) {
+    console.log('a 大於 b')
+  }
+}
