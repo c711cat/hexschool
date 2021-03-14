f79fab6e72823666ac8d36973acc194a43b4f6d6
c711cat <c711cat@gmail.com>
2021-03-14 20:48:29 +0800

    DOM課程複習-利用for loop將10個(及N個)p段落內容改為123

---
+++ js/DOM_review_0314_QA_2.js
@@ -0,0 +1,13 @@
+var titleClassEl = document.querySelectorAll('.titleclass')
+var titleClassElLen = titleClassEl.length
+var i
+for (i = 0; i < titleClassElLen; i++) {
+  titleClassEl[i].textContent = '123'
+}
+
+var moreParagraphEl = document.querySelectorAll('.moreparagraph')
+var moreParagraphElLen = moreParagraphEl.length
+var i
+for (i = 0; i < moreParagraphElLen; i++) {
+  moreParagraphEl[i].textContent = '123'
+}
