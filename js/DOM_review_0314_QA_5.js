1bd06c54d66f56e5f3068b9ba959b34b942e0322
c711cat <c711cat@gmail.com>
2021-03-16 11:16:50 +0800

    DOM課程複習-利用innerHTML及appendChild讓所有農夫名字顯示在網頁上
    第一次寫不出來先記錄起來

---
+++ js/DOM_review_0314_QA_5.js
@@ -0,0 +1,20 @@
+var farms = [
+  {
+    farmer: '卡斯柏',
+    dogs: ['張姆士', '龐德']
+  },
+  {
+    farmer: '查理',
+    dogs: ['皮皮']
+  }
+]
+var farmersNameEl = document.querySelector('.farmersName')
+var farmsLen = farms.length
+var str = ''
+var i
+
+for (i = 0; i < farmsLen; i++) {
+  str += list
+}
+console.log(str)
+farmersNameEl.innerHTML = farms[i].farmer
