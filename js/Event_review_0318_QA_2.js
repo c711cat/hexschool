bbd7190fbb01b07410d391331c650269b5ca7d34
c711cat <c711cat@gmail.com>
2021-03-22 15:24:14 +0800

    用onclick寫法-有及沒有按到alt時，跳出的視窗顯示有或沒有按到alt
    用 if 加上event.altKey == true  或  false方式去判斷顯示的視窗

---
+++ js/Event_review_0318_QA_2.js
@@ -0,0 +1,11 @@
+//用onclick寫法-有及沒有按到alt時，跳出的視窗顯示有或沒有按到alt
+var btnEl = document.querySelector('.btn_On')
+btnEl.onclick = function (e) {
+  if (e.altKey == true) {
+    alert('有按到alt')
+  }
+  if (e.altKey == false) {
+    alert('沒有按到alt')
+  }
+  console.log(e.altKey)
+}
