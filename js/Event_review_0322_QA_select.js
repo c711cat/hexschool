e41105fbd7885da607a4b1daa1ab908d67b64a45
c711cat <c711cat@gmail.com>
2021-03-22 18:02:12 +0800

    以陣列方式建立欲查詢的資料內容，使用querySelector綁定HTML的表單，用監聽語法寫觸發更換選單時所觸發的事件

---
+++ js/Event_review_0322_QA_select.js
@@ -0,0 +1,27 @@
+//使用網頁選單時，可隨選擇到的區域，以li的形式顯示出該區域的農夫名字
+var farms = [
+  {
+    farmer: '卡斯柏',
+    country: '前鎮區'
+  },
+  {
+    farmer: '查理',
+    country: '前鎮區'
+  },
+  {
+    farmer: '捷克',
+    country: '苓雅區'
+  }
+]
+
+var areaEl = document.querySelector('.area')
+var i
+var farmsLen = farms.length
+
+areaEl.addEventListener(
+  'change',
+  function (e) {
+    
+  },
+  false
+)
