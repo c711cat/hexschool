30088f779ba5627f1ffdf419cef8e37d400895a5
c711cat <c711cat@gmail.com>
2021-03-22 15:50:32 +0800

    用addEvevntListener寫法-有及沒有按到alt時，跳出的視窗顯示有或沒有按到alt
    addEvevntListener(' ....... ' , function.... , false) 寫法容易忘記第一個需要兩個上引號

---
+++ js/Event_review_0318_QA_3.js
@@ -0,0 +1,15 @@
+var btnEl = document.querySelector('.btn_Add')
+
+btnEl.addEventListener(
+  'click',
+  function (e) {
+    if (e.altKey == true) {
+      alert('有按到alt')
+    }
+    if (e.altKey == false) {
+      alert('沒有按到alt')
+    }
+    console.log(e.altKey)
+  },
+  false
+)
