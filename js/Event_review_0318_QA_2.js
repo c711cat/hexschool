34c3133ab66395402bd0bdd9f9c035f2185a77ef
c711cat <c711cat@gmail.com>
2021-03-23 12:39:53 +0800

    更改命名的習慣

--- js/Event_review_0318_QA_2.js
+++ js/Event_review_0318_QA_2.js
@@ -1,5 +1,5 @@
 //用onclick寫法-有及沒有按到alt時，跳出的視窗顯示有或沒有按到alt
-var btnEl = document.querySelector('.btn_On')
+var btnEl = document.querySelector('.btn-on')
 btnEl.onclick = function (e) {
   if (e.altKey == true) {
     alert('有按到alt')
