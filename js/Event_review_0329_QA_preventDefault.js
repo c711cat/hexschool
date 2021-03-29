8a8aed91e06997ff8bc0087812150e787ea5ac93
c711cat <c711cat@gmail.com>
2021-03-29 18:11:47 +0800

    HTML新增一個連結，並命名
    JS綁定HTML的連結命名，使用監聽方式點選連結時執行preventDefault讓網頁不會執行跳轉，但可以查看console中是否有執行

--- js/Event_review_0329_QA_preventDefault.js
+++ js/Event_review_0329_QA_preventDefault.js
@@ -1,1 +1,10 @@
 //說明e.preventDefault功能並舉例說明
+var linkEl = document.querySelector('.link')
+linkEl.addEventListener(
+  'click',
+  function (e) {
+    console.log('有執行但無跳轉網頁')
+    e.preventDefault()
+  },
+  false
+)
