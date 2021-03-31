aa912da91c0f5f5f16b6ae7b2f65a71fc170d940
c711cat <c711cat@gmail.com>
2021-03-31 14:55:53 +0800

    作業-說明e.stopPropagation功能並舉例說明-HTML先建好一個按鈕
    JS使用監聽方式藉由點按鈕時觸發2個視窗彈跳出來，再將e.stopPropagation插入兩個監聽之間

--- js/Event_review_0329_QA_stopPropagation.js
+++ js/Event_review_0329_QA_stopPropagation.js
@@ -1,1 +1,17 @@
 //說明e.stopPropagation功能並舉例說明
+var btnEl = document.querySelector('.btn')
+btnEl.addEventListener(
+  'click',
+  function (e) {
+    alert('add-1')
+  },
+  false
+)
+e.stopPropagation()
+btnEl.addEventListener(
+  'click',
+  function (e) {
+    alert('add-2')
+  },
+  false
+)
