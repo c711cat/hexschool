9d7790b956206136a494804aca80f9180a2b3101
c711cat <c711cat@gmail.com>
2021-04-08 15:07:22 +0800

    註解數字的意思，方便下次可以馬上看懂

--- js/keycode_homework .js
+++ js/keycode_homework .js
@@ -1,23 +1,24 @@
 var bodyEl = document.body
 
 function goRocket (e) {
+  console.log(e.keyCode)
   switch (e.keyCode) {
-    case 49:
+    case 49: //上方數字鍵1
       document.querySelector('.rocket1').style.bottom = '2000px'
       break
-    case 50:
+    case 50: //上方數字鍵2
       document.querySelector('.rocket2').style.bottom = '2000px'
       break
-    case 51:
+    case 51: //上方數字鍵3
       document.querySelector('.rocket3').style.bottom = '2000px'
       break
-    case 97:
+    case 97: //右邊數字鍵1
       document.querySelector('.rocket1').style.bottom = '2000px'
       break
-    case 98:
+    case 98: //右邊數字鍵2
       document.querySelector('.rocket2').style.bottom = '2000px'
       break
-    case 99:
+    case 99: //右邊數字鍵3
       document.querySelector('.rocket3').style.bottom = '2000px'
       break
   }
