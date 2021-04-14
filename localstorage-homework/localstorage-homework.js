fe368bfe28e1649c3d1e1817bcb2dbeda6bc3ff5
c711cat <c711cat@gmail.com>
2021-04-14 13:06:27 +0800

    第二次7成自己寫完-可執行版本

--- localstorage-homework/localstorage-homework.js
+++ localstorage-homework/localstorage-homework.js
@@ -1,49 +1,50 @@
 //1. 綁定DOM元素UL、btn、準備可以存進資料的陣列或空陣列
 
 //為什麼要綁定?
-//UL:為了指定後續的LI加在UL下方
+//UL:為了監聽指定a連結-刪除被點擊後可以觸發刪除LI的代辦事項
 //btn:為了監聽按下按鈕時可以觸發點擊事件去執行存資料進去localStorage中，
-//及以list形式顯示在網頁上
-//預備儲存的資料庫轉成陣列或空陣列:為了輸入的代辦事項可以陣列方式儲存進去，
+//及代辦事項以list形式顯示在網頁上
+//data:為了預備儲存的資料庫轉成陣列或空陣列:為了輸入的代辦事項可以陣列方式儲存進去，
 //方便後續取用在網頁上呈現
-
-var btn = document.querySelector('.btn')
-var ul = document.querySelector('.list')
-var data = JSON.parse(localStorage.getItem('toDoList')) || []
 
 //2. 三個function
 //(1)addList + addEventListener
 //用btn綁定監聽，點按鈕去觸發增加代辦事項進去localStorage中
 
-//(2)updateList
+//(2)updateList 預設每次都先更新資料庫
 //不須綁定，只要寫一個function放在對的位置去執行即可
 
 //(3)deleList + addEventListener
 //綁定ul，監聽ul中的刪除連結被點擊後，去執行刪除代辦事項的動作
 
+var btn = document.querySelector('.btn')
+var ul = document.querySelector('.list')
+var data = JSON.parse(localStorage.getItem('toDoList')) || []
+
 btn.addEventListener('click', addList, false)
 ul.addEventListener('click', deleList, false)
-updateList(data)
+updateList(data) //預設每次都更新一次
 
 function addList (e) {
-  e.preventDefault() //不寫此行會有送出的預設行為
+  e.preventDefault()
   var text = document.querySelector('.text').value
   var todo = { content: text }
-  console.log(data)
   data.push(todo)
+  var textStr = JSON.stringify(data)
   updateList(data)
-  localStorage.setItem('toDoList', JSON.stringify(data))
+  localStorage.setItem('toDoList', textStr)
 }
 
 function updateList (items) {
-  str = ''
+  //不懂洧杰這裡(data)為什麼要用items ?
+  var str = ''
   var len = items.length
   var i
   for (i = 0; i < len; i++) {
     str +=
-      '<li><a href="#" data-index=' +
+      '<li ><a href="#" data-num=' +
       i +
-      '>刪除</a><span>' +
+      '>刪除</a> <span>' +
       items[i].content +
       '</span></li>'
   }
@@ -51,12 +52,13 @@ function updateList (items) {
 }
 
 function deleList (e) {
-  e.preventDefault() //不寫此行會有跳轉頁面行為
+  e.preventDefault()
+
   if (e.target.nodeName !== 'A') {
     return
   }
-  var index = e.target.dataset.index
-  data.splice(index, 1)
+  var num = e.target.dataset.num
+  data.splice(num, 1)
   localStorage.setItem('toDoList', JSON.stringify(data))
   updateList(data)
 }
