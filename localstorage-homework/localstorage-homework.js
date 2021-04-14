0d651aec47b9ab0695c7917aa19f99daf389623e
c711cat <c711cat@gmail.com>
2021-04-14 18:38:56 +0800

    綁定UL，再讓localStorage的代辦事項可以呈現在網頁上

--- localstorage-homework/localstorage-homework.js
+++ localstorage-homework/localstorage-homework.js
@@ -10,8 +10,6 @@
 //讓輸入的文字可以物件形式存入
 //可讓代辦事項的物件新增進去空陣列中
 
-
-
 //單獨一個function updateList
 //使用方法:以呼叫function去執行
 //function(參數)，參數是data，data是輸入的代辦事項，將參數命名為items(items=data)
@@ -23,6 +21,7 @@
 //代辦事項的文字，從存在LS的陣列中取得，陣列名稱為data，等於帶入的參數items
 //所以items[0].content，就是存在LS中的第一個物件內容
 //為了讓每次重新整理網頁時、新增及刪除代辦事項時，都會自動更新一次代辦事項內容
+//先綁定UL，再讓localStorage的代辦事項可以呈現在網頁上
 
 //4.按下A連結的刪除時，可以同時刪除網頁上及localStorage的代辦事項
 //綁定UL去監聽按了連結的刪除後，觸發刪除的行為
@@ -35,22 +34,29 @@
 //重新更新一次LS及更新一次網頁上的畫面
 
 var btn = document.querySelector('.btn')
+var ul = document.querySelector('.list')
 var data = []
+
 btn.addEventListener('click', addList, false)
+updateList(data)
+
 function addList (e) {
   e.preventDefault()
   var text = document.querySelector('.text').value
   var object = { content: text }
   data.push(object)
+
   var textStr = JSON.stringify(object)
-  localStorage.setItem('toDoList', textStr)
   updateList(data)
+  localStorage.setItem('toDoList', textStr)
 }
 
 function updateList (items) {
   var str = ''
   var i
   for (i = 0; i < items.length; i++) {
-  str+='<li><a href="#">刪除</a><span>'+items[i].content+'</span></li>'
+    str += '<li><a href="#">刪除</a><span>' + items[i].content + '</span></li>'
   }
+  JSON.parse(localStorage.getItem('toDoList'))
+  ul.innerHTML = str
 }
