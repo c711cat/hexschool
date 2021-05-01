60e1f4382058586b54e3f00d8e87f502b8d91c81
c711cat <c711cat@gmail.com>
2021-05-01 16:43:00 +0800

    需要再確定資料是否回傳正確，要確定status : 200  的寫法

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -3,7 +3,10 @@ xhr.open('get', 'https://hexschool.github.io/ajaxHomework/data.json', true)
 xhr.send(null)
 xhr.onload = function () {
   console.log(xhr.responseText)
-  var str = JSON.parse(xhr.responseText)
-  var txt = (document.querySelector('.message').textContent = str[0].name)
-  console.log(txt)
+  if (xhr.status == 200) {
+    var str = JSON.parse(xhr.responseText)
+    document.querySelector('.message').textContent = str[0].name
+  } else {
+    console.log('資料錯誤')
+  }
 }
