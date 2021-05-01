6ab405acf8c85a49f2b616cafb6a1862e8c2b45a
c711cat <c711cat@gmail.com>
2021-05-01 16:20:33 +0800

    將資料顯示到網頁上

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -1,3 +1,9 @@
 var xhr = new XMLHttpRequest()
 xhr.open('get', 'https://hexschool.github.io/ajaxHomework/data.json', true)
 xhr.send(null)
+xhr.onload = function () {
+  console.log(xhr.responseText)
+  var str = JSON.parse(xhr.responseText)
+  var txt = (document.querySelector('.message').textContent = str[0].name)
+  console.log(txt)
+}
