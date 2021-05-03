e577bf3c4e2d0940e47fb242a9cbdb814e39fdfe
c711cat <c711cat@gmail.com>
2021-05-03 13:34:49 +0800

    登入帳號密碼的AJAX作業

--- AJAX/AJAX-POST-JSON-homework.js
+++ AJAX/AJAX-POST-JSON-homework.js
@@ -20,3 +20,30 @@ function singup () {
     }
   }
 }
+
+var signinBtn = document.querySelector('.signin-btn')
+signinBtn.addEventListener('click', signin, false)
+function signin () {
+  var signinEmailStr = document.querySelector('.signin-email').value
+  var signinPasswordStr = document.querySelector('.signin-password').value
+  var signinAccount = {}
+  signinAccount.email = signinEmailStr
+  signinAccount.password = signinPasswordStr
+  var signinXhr = new XMLHttpRequest()
+  signinXhr.open(
+    'post',
+    'https://hexschool-tutorial.herokuapp.com/api/signin',
+    true
+  )
+  signinXhr.setRequestHeader('Content-type', 'application/json')
+  signinXhr.send(JSON.stringify(signinAccount))
+  signinXhr.onload = function () {
+    var signinCallback = JSON.parse(signinXhr.responseText)
+    console.log(signinCallback)
+    if (signinCallback.message == '登入成功') {
+      alert('登入成功')
+    } else {
+      alert('"此帳號不存在或帳號密碼錯誤')
+    }
+  }
+}
